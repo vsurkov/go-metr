b5356e1eee4dfb87508ce7ca727a1f5108b6dc11
Surkov Vladimir <surkovv@gmail.com>
2022-08-28 14:02:23 +0300

    Порадок в файлах сборки

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
+#Заметки на полях для сборки и запуска
 
+#Сборка бэкенда
 #rest-receiver
 docker build -t surkovvs/go-metr-receiver --build-arg PRODUCT_NAME=rest-receiver --build-arg PRODUCT_VER=0.0.48 -f ./build/Dockerfile .
+#docker push surkovvs/go-metr-receiver
+
 #clickhouse-sender
 docker build -t surkovvs/go-metr-sender --build-arg PRODUCT_NAME=clickhouse-sender --build-arg PRODUCT_VER=0.0.48 -f ./build/Dockerfile .
+#docker push surkovvs/go-metr-sender
